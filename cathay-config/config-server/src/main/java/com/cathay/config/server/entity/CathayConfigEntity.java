2164f4c8a25b1cccf18459c570380abc8cc105ef
chenkang <571899315@qq.com>
2019-01-28 20:30:16 +0800

    提交jar修改

--- cathay-config/config-server/src/main/java/com/cathay/config/server/entity/CathayConfigEntity.java
+++ cathay-config/config-server/src/main/java/com/cathay/config/server/entity/CathayConfigEntity.java
@@ -1,13 +1,48 @@
 package com.cathay.config.server.entity;
 
 import com.cathay.mybatis.core.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Date;
 
+@Getter
+@Setter
 public class CathayConfigEntity extends BaseEntity {
 
 
     public Serializable getId() {
         return null;
     }
+
+
+    /** 主键 */
+    private Integer id;
+
+    /** 应用名称 */
+    private String appName;
+
+    /** 应用环境名称 */
+    private String appEnv;
+
+    /** key */
+    private String key;
+
+    /** value */
+    private String value;
+
+    /** 注释 */
+    private String comment;
+
+    /**  */
+    private Date gmtCreated;
+
+    /**  */
+    private Date gmtModified;
+
+
+
+
+
 }
