9be359a5bdb89b383c71619d89cc25c5a252ee0c
571899315@qq.com <571899315@qq.com>
2019-03-01 17:23:35 +0800

    commit

--- cathay-config/config-server/src/main/java/com/cathay/config/server/config/ServerInit.java
+++ cathay-config/config-server/src/main/java/com/cathay/config/server/config/ServerInit.java
@@ -1,5 +1,7 @@
 package com.cathay.config.server.config;
 
+import com.cathay.config.server.netty.ConfigServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,11 +10,12 @@ import javax.annotation.PostConstruct;
 public class ServerInit {
 
 
+    @Autowired
+    private ConfigServer configServer;
+
+
     @PostConstruct
     public void init() {
-
-
-
 
 
     }
