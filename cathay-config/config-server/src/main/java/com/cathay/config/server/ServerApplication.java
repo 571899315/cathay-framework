6a5421f4870660d43d509f53b19c75f0329a5d2a
571899315@qq.com <571899315@qq.com>
2019-03-01 15:30:48 +0800

    commit

--- cathay-config/config-server/src/main/java/com/cathay/config/server/ServerApplication.java
+++ cathay-config/config-server/src/main/java/com/cathay/config/server/ServerApplication.java
@@ -5,10 +5,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.cathay")
 @ComponentScan(value = {"com.cathay"})
+@ImportResource({"classpath*:spring/*.xml"})
 public class ServerApplication {
 
     public static void main(String[] args) {
